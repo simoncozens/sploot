c81ff07ef455c02e8af458f99d526fbd3a9a906d
Simon Cozens <simon@simon-cozens.org>
2024-06-27 17:43:13 +0100

    Toggle live reloading

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,12 +1,35 @@
-import * as Bootstrap from 'bootstrap'
+import * as Bootstrap from "bootstrap";
 import $ from "jquery";
 import hbjs from "./hbjs";
-import {Font } from "./font";
-import { fontlist} from "./fontlist";
+import { Font } from "./font";
+import { fontlist } from "./fontlist";
 import "./colortoggle";
 import "./sidebar";
 
 window["$"] = $;
+
+$(() => {
+  $('[data-toggle="tooltip"]').each((i, el) => {
+    new Bootstrap.Tooltip(el);
+  });
+  // Disable live reload if there's no FileSystem API
+  if (!("showOpenFilePicker" in window)) {
+    $("#live-reload svg use").attr("href", "#lightning-charge");
+    $("#live-reload").parent().hide();
+  } else {
+    $("#live-reload").on("click", () => {
+      if (liveReloading()) {
+        $("#live-reload svg use").attr("href", "#lightning-charge");
+      } else {
+        $("#live-reload svg use").attr("href", "#lightning-charge-fill");
+      }
+    });
+  }
+});
+
+function liveReloading() {
+  return $("#live-reload svg use").attr("href") == "#lightning-charge-fill";
+}
 
 fetch(`assets/harfbuzz.wasm`)
   .then((response) => response.arrayBuffer())
@@ -18,19 +41,18 @@ fetch(`assets/harfbuzz.wasm`)
     window["hbjs"] = hb;
   });
 
-
-var handler = $('.handler');
+var handler = $(".handler");
 var isHandlerDragging = false;
 
-handler.on('mousedown', function(e) {
+handler.on("mousedown", function (e) {
   isHandlerDragging = true;
 });
 
-handler.on('mouseup', function(e) {
+handler.on("mouseup", function (e) {
   isHandlerDragging = false;
-})
+});
 
-handler.on('mousemove', function(e) {
+handler.on("mousemove", function (e) {
   if (!isHandlerDragging) {
     return false;
   }
@@ -39,11 +61,12 @@ handler.on('mousemove', function(e) {
   var pointerRelativeXpos = e.clientX - containerOffsetLeft;
   var boxAminWidth = 60;
 
-  boxBefore[0].style.width = (Math.max(boxAminWidth, pointerRelativeXpos - 8)) + 'px';
+  boxBefore[0].style.width =
+    Math.max(boxAminWidth, pointerRelativeXpos - 8) + "px";
   boxBefore[0].style.flexGrow = "0";
 });
 
-$("#charlist-toggle-item").on("click", function() {
+$("#charlist-toggle-item").on("click", function () {
   let state = $("#charlist").css("display") == "none";
   $("#charlist").toggle();
   $("#charlist-handle").toggle();
@@ -51,7 +74,7 @@ $("#charlist-toggle-item").on("click", function() {
     state ? "Hide Character List" : "Show Character List"
   );
 });
-$("#glyphlist-toggle-item").on("click", function() {
+$("#glyphlist-toggle-item").on("click", function () {
   let state = $("#glyphlist").css("display") == "none";
   $("#glyphlist").toggle();
   $("#glyphlist-handle").toggle();
@@ -64,44 +87,66 @@ $("#glyphlist-toggle-item").on("click", function() {
 
 $("#text").on("input", fontlist.updateText.bind(fontlist));
 
+async function standardLoad(item: DataTransferItem) {
+  // Safari
+  var reader = new FileReader();
+  reader.onload = (function (file: File) {
+    return (e) => {
+      var arrayBuffer = reader.result as ArrayBuffer;
+      var byteArray = new Uint8Array(arrayBuffer);
+      var newFont = new Font(undefined);
+      fontlist.fonts.push(newFont);
+      newFont.familyname = "font" + fontlist.fonts.length;
+      newFont.init(byteArray);
+      newFont.filename = file.name;
+      fontlist.update();
+    };
+  })(item.getAsFile());
+  reader.readAsArrayBuffer(item.getAsFile());
+}
+
+async function liveReload(item: DataTransferItem) {
+  // Chrome and friends
+  let handle: FileSystemHandle = await item.getAsFileSystemHandle();
+  let font = new Font(handle as FileSystemFileHandle);
+  setInterval(font.compare.bind(font), 1000);
+}
+
 async function handleUpload(files: DataTransferItemList) {
   for (var item of files) {
     // Is it a TTF/OTF?
-    if (item.kind == "file" && (item.type == "font/ttf" || item.type == "font/otf")) {
-      try {
-        // Chrome and friends
-        let handle: FileSystemHandle = await item.getAsFileSystemHandle();
-        let font = new Font(handle as FileSystemFileHandle)
-        setInterval(font.compare.bind(font), 1000);
-      } catch (e) {
-        // Safari
-        var reader = new FileReader();
-        reader.onload = (function (file: File) {
-          return (e) => {
-            var arrayBuffer = reader.result as ArrayBuffer;
-            var byteArray = new Uint8Array(arrayBuffer);
-            var newFont = new Font(undefined);
-            fontlist.fonts.push(newFont);
-            newFont.familyname = "font" + fontlist.fonts.length;
-            newFont.init(byteArray);
-            newFont.filename = file.name;
-            fontlist.update();
-          }
-        })(item.getAsFile());
-        reader.readAsArrayBuffer(item.getAsFile());
+    if (
+      item.kind == "file" &&
+      (item.type == "font/ttf" || item.type == "font/otf")
+    ) {
+      console.log("Live reloading?", liveReloading());
+      if (liveReloading()) {
+        try {
+          liveReload(item);
+        } catch (e) {
+          standardLoad(item);
+        }
+      } else {
+        standardLoad(item);
       }
     } else {
-      $("#toast .toast-body").html("Invalid file type: " + item.getAsFile().type);
-      (new Bootstrap.Toast($("#toast")[0])).show();
+      $("#toast .toast-body").html(
+        "Invalid file type: " + item.getAsFile().type
+      );
+      new Bootstrap.Toast($("#toast")[0]).show();
     }
   }
 }
 
 // Drop zone handling
-$("#main").on("dragover", function(e) { e.preventDefault(); });
-$("#main").on("dragenter", function(e) { e.preventDefault(); });
-$("#main").on("drop", function(e) {
-  console.log("Drop on main")
+$("#main").on("dragover", function (e) {
+  e.preventDefault();
+});
+$("#main").on("dragenter", function (e) {
+  e.preventDefault();
+});
+$("#main").on("drop", function (e) {
+  console.log("Drop on main");
   e.preventDefault();
   e.stopPropagation();
   handleUpload(e.originalEvent.dataTransfer.items);
